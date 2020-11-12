7df0c30f70a0d49f0ce12e6c91cabf19baacff0f
val <8445610+valmack@users.noreply.github.com>
2020-11-12 12:59:23 -0500

    fmt: check boolean env vars are string true

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns true if `value` is the string `"true"`.
- * Returns false otherwise.
- * @param value String version of the boolean
- */
-export function toBoolean(value: string | undefined): boolean {
-    return (value === "true")
-}
