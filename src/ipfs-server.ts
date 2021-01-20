cf7465ed6e0ef382e473b50b0e62d013d38366df
Zach Ferland <zachferland@gmail.com>
2021-01-19 20:34:05 -0500

    fix: disable preload

--- src/ipfs-server.ts
+++ src/ipfs-server.ts
@@ -78,6 +78,9 @@ export default class IPFSServer {
                     announce: announceAddresses,
                 }
             },
+            preload: {
+                enabled: false
+            },
             config: {
                 Addresses: {
                     Swarm: [
